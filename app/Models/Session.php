19a18706390ec78fd17704d726d2c74491f9252b
mikelji97 <mikel.falcesino@gmail.com>
2026-01-10 00:28:14 +0100

    create session model

--- app/Models/Session.php
+++ app/Models/Session.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Session extends Model
 {
-    //
+    protected $table = 'gym_sessions';
+
+    protected $fillable = [
+        'gym_class_id',
+        'date',
+        'start_time',
+        'end_time',
+        'room',
+        'max_capacity',
+        'current_bookings',
+    ];
 }
