6634fb4762b3c156d1c11f5533cc0e2d158c954e
mikelji97 <mikel.falcesino@gmail.com>
2026-01-10 02:32:37 +0100

    fix sessions model, create and try to fail session index test and add route.

--- routes/api.php
+++ routes/api.php
@@ -3,15 +3,19 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\GymClassController;
+use App\Http\Controllers\SessionController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    //Gym class
     Route::get('/gym-classes', [GymClassController::class, 'index']);
     Route::post('/gym-classes', [GymClassController::class, 'store']);
     Route::get('/gym-classes/{id}', [GymClassController::class, 'show']);
     Route::put('/gym-classes/{id}', [GymClassController::class, 'update']);
     Route::delete('/gym-classes/{id}', [GymClassController::class, 'destroy']);
+    //Gym sessions   
+    Route::get('/sessions', [SessionController::class, 'index']);
 });
