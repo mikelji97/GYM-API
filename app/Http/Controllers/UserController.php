2569b63b6b604824eed70110067d8bbe21b3a5eb
mikelji97 <mikel.falcesino@gmail.com>
2026-01-12 02:22:59 +0100

    Create index controller and try to pass test

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        $users = User::all();
+        return response()->json(['data' => $users], 200);
+    }
 }
