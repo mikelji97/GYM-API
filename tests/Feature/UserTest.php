135e76c6ffaccd3ce56880915b2292259e840274
mikelji97 <mikel.falcesino@gmail.com>
2026-01-12 01:36:31 +0100

    Create userTest and add setUp function

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Models\User;
+use Laravel\Passport\Passport;
+
+class UserTest extends TestCase
+{
+    use RefreshDatabase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Passport::loadKeysFrom(storage_path());
+
+        \DB::table('oauth_clients')->insert([
+            'id' => \Illuminate\Support\Str::uuid(),
+            'owner_type' => null,
+            'owner_id' => null,
+            'name' => 'Test Personal Access Client',
+            'secret' => null,
+            'provider' => 'users',
+            'redirect_uris' => json_encode(['http://localhost']),
+            'grant_types' => json_encode(['personal_access']),
+            'revoked' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
+    }
+}
