b7d2eeb1a21546b12e0b64324abb368be6633dea
mikelji97 <mikel.falcesino@gmail.com>
2026-01-18 23:06:52 +0100

    Add Passport config to BookingTest

--- tests/Feature/BookingTest.php
+++ tests/Feature/BookingTest.php
@@ -3,18 +3,33 @@
 namespace Tests\Feature;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Session;
+use App\Models\Booking;
+use Laravel\Passport\Passport;
 
 class BookingTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    protected function setUp(): void
+    {
+        parent::setUp();
+        Passport::loadKeysFrom(storage_path());
+
+        \DB::table('oauth_clients')->insert([
+            'id' => \Illuminate\Support\Str::uuid(),
+            'owner_type' => null,
+            'owner_id' => null,
+            'name' => 'Test Personal Access Client',
+            'secret' => null,
+            'provider' => 'users',
+            'redirect_uris' => json_encode(['http://localhost']),
+            'grant_types' => json_encode(['personal_access']),
+            'revoked' => false,
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
 }
