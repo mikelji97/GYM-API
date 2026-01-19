693c4200b39687538282c8fbd9f0ce2787e6344b
mikelji97 <mikel.falcesino@gmail.com>
2026-01-19 01:42:27 +0100

    add route for store endpoint

--- routes/api.php
+++ routes/api.php
@@ -34,4 +34,5 @@ Route::middleware('auth:api')->group(function () {
     //bookings
     Route::get('/bookings/my-bookings', [BookingController::class, 'myBookings']);
     Route::get('/bookings', [BookingController::class, 'index']);
+    Route::post('/bookings', [BookingController::class, 'store']);
 });
